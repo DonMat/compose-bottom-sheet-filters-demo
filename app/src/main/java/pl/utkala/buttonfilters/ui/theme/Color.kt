5ba4e7c0e69dbdfe6d3583a653debac9f8d3049a
Mateusz Utkała <mateusz@utkala.pl>
2022-12-29 10:38:42 +0100

    Initial commit

---
+++ app/src/main/java/pl/utkala/buttonfilters/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package pl.utkala.buttonfilters.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Green500 = Color(0xFF8ac148)
+val Green700 = Color(0xFF599014)
+val Green200 = Color(0xFFbdf478)
