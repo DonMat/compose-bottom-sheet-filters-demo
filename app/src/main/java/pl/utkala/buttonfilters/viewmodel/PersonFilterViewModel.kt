aa7f09eee9c7ac212ee1c8d23ca5f76d60c1d989
Mateusz Utkała <mateusz@utkala.pl>
2022-12-31 13:39:02 +0100

    Move people list to ViewModel to improve readability

--- app/src/main/java/pl/utkala/buttonfilters/viewmodel/PersonFilterViewModel.kt
+++ app/src/main/java/pl/utkala/buttonfilters/viewmodel/PersonFilterViewModel.kt
@@ -5,10 +5,21 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import pl.utkala.buttonfilters.data.FilterablePerson
+import pl.utkala.buttonfilters.model.Job
 import pl.utkala.buttonfilters.model.Person
-import pl.utkala.buttonfilters.model.people
 
 class PersonFilterViewModel : ViewModel() {
+
+    private val people: List<Person> = listOf<Person>(
+        Person("E0001", "John", "Doe", Job.PROGRAMMER),
+        Person("E0002", "Ellen", "Cunningham", Job.PROGRAMMER),
+        Person("E0003", "Carmen", "Walker", Job.TESTER),
+        Person("E0004", "Mike", "Walker", Job.PROGRAMMER),
+        Person("E0005", "Edgar", "Bourn", Job.WEB_DEVELOPER),
+        Person("E0006", "Richard", "Robson", Job.WEB_DEVELOPER),
+        Person("E0007", "Ralph", "Poe", Job.WEB_DEVELOPER),
+        Person("E0008", "Max", "Smith", null)
+    )
 
     private val _filteredList = MutableStateFlow<List<Person>>(people)
     val list: StateFlow<List<Person>> = _filteredList
